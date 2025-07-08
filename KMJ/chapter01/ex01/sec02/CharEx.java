f845cb07b2aebd85009986c9788614326ff56781
MyeongJun0911 <kmyeongjun124@gmail.com>
2025-07-08 21:21:22 +0900

    add: day1 example

---
+++ KMJ/chapter01/ex01/sec02/CharEx.java
@@ -0,0 +1,17 @@
+package KMJ.chapter01.ex01.sec02;
+
+public class CharEx {
+    public static void main(String[] args) {
+        // char형 변수 character1를 문자 'A'로 초기화
+        char character1 = 'A';
+
+        // char형 변수 character2를 정수(리터럴)65로 초기화
+        char character2 = 65;
+
+        // ''빈 문자입력시 컴파일 오류
+        // char character3 = '';
+
+        System.out.println(character1); // A
+        System.out.println(character2); // A
+    }
+}
